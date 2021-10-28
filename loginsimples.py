3816986cd5e22d8adaadf6e680e9e025c50e2ad6
DIEGO MALTA GOUVEIA <diego.pel@hotmail.com>
2021-10-28 00:28:57 -0300

    aprimorando codigo loginsimples.py
    adicionei a limpeza de tela e o delay

--- loginsimples.py
+++ loginsimples.py
@@ -1,4 +1,5 @@
 # login simples 0.1
+from time import sleep
 banco_de_dados = [
     'Admin', 'admin'
 ]
@@ -6,19 +7,27 @@ login = ''
 senha = ''
 
 
+def limpa():
+    print('\n'*100)
+
+
 def CampoDeLogin():
     global banco_de_dados
     global login
     global senha
     login_seguranca = False
+    limpa()
     while not login_seguranca:
         login = input('Digite seu Usuario: ')
         senha = input('Digite sua senha: ')
+        limpa()
         if login in banco_de_dados[0] and senha in banco_de_dados[1]:
             print('Login realizado com sucesso!')
             login_seguranca = True
         else:
             print('Usuario e/ou senha incorretos! tente novamente')
+        sleep(2)
+        limpa()
     return
 
 
