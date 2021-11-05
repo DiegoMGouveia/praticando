5139cd829e1bac9953bc5c9b1fceba8360eb3b0f
DIEGO MALTA GOUVEIA <diego.pel@hotmail.com>
2021-11-05 14:46:33 -0300

    criado sorteiodepalavras.py
    script que utiliza função, dicionário e a função randint para escolher a palavra dentro da categoria

---
+++ sorteiodepalavra.py
@@ -0,0 +1,35 @@
+#criei esse sisteminha de palavras aleatórias dentro de dicionários
+#afim de praticar o uso de funções e dicionários
+
+from random import randint
+
+biblioteca = {
+    'comida': {
+        1: 'Banana',
+        2: 'Guisado',
+        3: 'arroz',
+        4: 'espinafre',
+        5: 'Feijão',
+        6: 'Panqueca',
+    },
+    'bebida': {
+        1: 'Leite',
+        2: 'Café',
+        3: 'Chá',
+        4: 'Refrigerante',
+        5: 'Chimarrão',
+        6: 'Suco',
+    },
+}
+categoria = list(biblioteca)
+
+
+def sortear(args):
+    palavra = args[randint(1, 6)]
+    return palavra
+
+
+tema_escolhida = input(f'Escolha  uma categoria {categoria}: ')
+palavra = sortear(biblioteca[categoria_escolhida])
+print(f'Categoria escolhida: {categoria_escolhida}\n '
+      f'Palavra sorteada: {palavra}')
